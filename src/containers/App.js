a306bc655c257e7f1b8ef2baf28edf93d4c895d2
Alexander Terentyev <olek.terentyev@gmail.com>
2021-11-11 18:25:42 +0100

    Added ErrorBoundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import Scroll from '../components/Scroll';
-import './App.css';
+import Scroll from "../components/Scroll";
+import "./App.css";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = { //<== smart component
+    super();
+    this.state = {
+      //<== smart component
       robots: [],
-      searchfield: ''
-    }
+      searchfield: "",
+    };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users =>this.setState({robots: users}))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
-  }
-
-
+  };
 
   render() {
-    const {robots , searchfield} = this.state;
-    const filteredRobots = robots.filter(robot => {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
+    });
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-        <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
